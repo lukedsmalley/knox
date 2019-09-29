f6d0334d77dd526a79baca329ca7e7e5d70d0450
Luke Smalley <lukedsmalley@gmail.com>
2019-09-29 07:33:23 -0400

    Update presentation data

--- knox-serve/src/index.ts
+++ knox-serve/src/index.ts
@@ -7,20 +7,52 @@ app.get('/tools', (request, response) => {
     {
       name: 'ESLint',
       role: 'Linter'
-    }
+    },
+    {
+      name: 'Gulp',
+      role: 'Build Tool'
+    },
+    {
+      name: 'Docker',
+      role: 'Containerization'
+    },
+    {
+      name: 'NPM',
+      role: 'Package Manager'
+    },
+    {
+      name: 'Travis',
+      role: 'Continuous Integration'
+    },
+    {
+      name: 'Git',
+      role: 'Source Control'
+    },
+    {
+      name: 'TypeScript',
+      role: 'Compiler'
+    },
   ]))
 })
 
 app.get('/abstractions', (request, response) => {
   response.send(JSON.stringify([
     ['Compiler', 'Parser', 'Scanner'],
-    ["Program", 'SyntaxKind'],
-    ['func', 'd']
+    ["SignatureFlags", 'SyntaxKind', 'parseJsonText'],
+    ['parseIsolatedEntityName', 'isExternalModule'],
+    ['updateSourceFile', 'createNode', 'NodeConstructor'],
+    ['TokenConstructor', 'IdentifierConstructor', 'visitNode<T>']
   ]))
 })
 
 app.get('/procedure', (request, response) => {
-  response.send('no code')
+  response.send(`//Translation of application code back into a single, long procedure
+  //Starting in main() or wherever appropriate
+  let config = loadConfig()
+  ...
+  while (true) {
+    mainLoop()
+  }`)
 })
 
 app.listen(3001, () => console.log('Started'))
